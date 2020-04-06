f9cf69e0fe75503d84990d06d0eb993f4e7a888d
Felicià MAVIANE <maviane@gmail.com>
2020-04-06 13:48:17 +0200

    - Added notebooks for new sources (WIP) - Fixed main form disposition

---
+++ dw_cssegis_data.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import numpy as np
+
+root_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
+confirmed_url = root_url + "time_series_covid19_confirmed_global.csv"
+dead_url = root_url + "time_series_covid19_deaths_global.csv"
+recovered_url = root_url + "time_series_covid19_recovered_global.csv"
+
+
+def get_data_and_meld(url: str, val_name: str) -> pd.DataFrame:
+    """Prepare a dataframe using an URL and a name for the column
+    
+    Arguments:
+        url {str} -- github raw link
+        val_name {str} -- name of the column
+    
+    Returns:
+        DataFrame -- Built dataframe
+    """
+    return (
+        pd.read_csv(url, error_bad_lines=False)
+        .rename(
+            columns={
+                "Province/State": "province_state",
+                "Country/Region": "country_region",
+                "Lat": "lat",
+                "Long": "long",
+            }
+        )
+        .melt(
+            id_vars=["province_state", "country_region", "lat", "long"],
+            var_name="date",
+            value_name=val_name,
+        )
+    )
+
+
+def get_wrangled_cssegis_df():
+    # build the three root dataframes
+    df_confirmed = get_data_and_meld(url=confirmed_url, val_name="confirmed")
+    df_dead = get_data_and_meld(url=dead_url, val_name="dead")
+    df_recovered = get_data_and_meld(url=recovered_url, val_name="recovered")
+
+    # Merge dataframes
+    df_merged = (
+        df_confirmed.join(df_dead["dead"])
+        .join(df_recovered["recovered"])
+        .sort_values(["country_region", "date"])
+        .reset_index()
+        .drop(columns=["index"])
+    )
+    df_merged.date = pd.to_datetime(df_merged.date)
+
+    # Build world dataframe and add it to the mix
+    df_world = (
+        df_merged.drop(columns=["province_state", "country_region", "lat", "long"])
+        .set_index("date")
+        .resample("d")
+        .sum(min_count=180)
+        .reset_index()
+        .assign(country_region="World", lat=np.nan, long=np.nan)
+    )
+    df_all = pd.concat((df_merged, df_world)).sort_values(["country_region", "date"])
+
+    # Remove regions
+    df_no_region = (
+        df_all.drop(columns=["province_state"])
+        .groupby(["country_region", "date"])
+        .sum()
+        .reset_index()
+    )
+
+    # Add columns
+    df_new_column = (
+        df_no_region.assign(confirmed_total=df_no_region.confirmed)
+        .assign(recovered_total=df_no_region.recovered)
+        .assign(dead_total=df_no_region.dead)
+        .assign(country=df_no_region.country_region)
+        .assign(
+            confirmed_daily=df_no_region.confirmed.subtract(
+                df_no_region.confirmed.shift(1), fill_value=0
+            ).clip(0)
+        )
+        .assign(
+            recovered_daily=df_no_region.recovered.subtract(
+                df_no_region.recovered.shift(1), fill_value=0
+            ).clip(0)
+        )
+        .assign(
+            dead_daily=df_no_region.dead.subtract(
+                df_no_region.dead.shift(1), fill_value=0
+            ).clip(0)
+        )
+        .drop(columns=["confirmed", "recovered", "dead", "country_region"])
+    )
+
+    df = df_new_column[
+        [
+            "country",
+            "date",
+            "lat",
+            "long",
+            "confirmed_total",
+            "confirmed_daily",
+            "recovered_total",
+            "recovered_daily",
+            "dead_total",
+            "dead_daily",
+        ]
+    ]
+
+    # Set recovered to int
+    df.recovered_total = df.recovered_total.fillna(-1)
+    df.recovered_total = pd.to_numeric(df.recovered_total, downcast="integer")
+    df.recovered_total = df.recovered_total.replace(-1, np.nan)
+
+    return df
--- yacd.py
+++ yacd.py
@@ -12,118 +12,11 @@ import dash_bootstrap_components as dbc
 
 import plotly.express as px
 
-confirmed_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-dead_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
-recovered_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+from dw_cssegis_data import get_wrangled_cssegis_df
+
+df = get_wrangled_cssegis_df()
 
 
-def get_data_and_meld(url: str, val_name: str) -> pd.DataFrame:
-    """Prepare a dataframe using an URL and a name for the column
-    
-    Arguments:
-        url {str} -- github raw link
-        val_name {str} -- name of the column
-    
-    Returns:
-        DataFrame -- Built dataframe
-    """
-    return (
-        pd.read_csv(url, error_bad_lines=False)
-        .rename(
-            columns={
-                "Province/State": "province_state",
-                "Country/Region": "country_region",
-                "Lat": "lat",
-                "Long": "long",
-            }
-        )
-        .melt(
-            id_vars=["province_state", "country_region", "lat", "long"],
-            var_name="date",
-            value_name=val_name,
-        )
-    )
-
-
-# build the three root dataframes
-df_confirmed = get_data_and_meld(url=confirmed_url, val_name="confirmed")
-df_dead = get_data_and_meld(url=dead_url, val_name="dead")
-df_recovered = get_data_and_meld(url=recovered_url, val_name="recovered")
-
-# Merge dataframes
-df_merged = (
-    df_confirmed.join(df_dead["dead"])
-    .join(df_recovered["recovered"])
-    .sort_values(["country_region", "date"])
-    .reset_index()
-    .drop(columns=["index"])
-)
-df_merged.date = pd.to_datetime(df_merged.date)
-
-# Build world dataframe and add it to the mix
-df_world = (
-    df_merged.drop(columns=["province_state", "country_region", "lat", "long"])
-    .set_index("date")
-    .resample("d")
-    .sum(min_count=180)
-    .reset_index()
-    .assign(country_region="World", lat=np.nan, long=np.nan)
-)
-df_all = pd.concat((df_merged, df_world)).sort_values(["country_region", "date"])
-
-# Remove regions
-df_no_region = (
-    df_all.drop(columns=["province_state"])
-    .groupby(["country_region", "date"])
-    .sum()
-    .reset_index()
-)
-
-# Add columns
-df_new_column = (
-    df_no_region.assign(confirmed_total=df_no_region.confirmed)
-    .assign(recovered_total=df_no_region.recovered)
-    .assign(dead_total=df_no_region.dead)
-    .assign(country=df_no_region.country_region)
-    .assign(
-        confirmed_daily=df_no_region.confirmed.subtract(
-            df_no_region.confirmed.shift(1), fill_value=0
-        ).clip(0)
-    )
-    .assign(
-        recovered_daily=df_no_region.recovered.subtract(
-            df_no_region.recovered.shift(1), fill_value=0
-        ).clip(0)
-    )
-    .assign(
-        dead_daily=df_no_region.dead.subtract(df_no_region.dead.shift(1), fill_value=0).clip(0)
-    )
-    .drop(columns=["confirmed", "recovered", "dead", "country_region"])
-)
-
-df = df_new_column[
-    [
-        "country",
-        "date",
-        "lat",
-        "long",
-        "confirmed_total",
-        "confirmed_daily",
-        "recovered_total",
-        "recovered_daily",
-        "dead_total",
-        "dead_daily",
-    ]
-]
-
-# Set recovered to int
-df.recovered_total = df.recovered_total.fillna(-1)
-df.recovered_total = pd.to_numeric(df.recovered_total, downcast="integer")
-df.recovered_total = df.recovered_total.replace(-1, np.nan)
-
-# Begin Dashboard
-# external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-# external_stylesheets = ["https://codepen.io/anon/pen/mardKv.css"]
 external_stylesheets = dbc.themes.BOOTSTRAP
 
 # app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -137,24 +30,13 @@ layout_style = {
 text_layout = layout_style.copy()
 text_layout["text-align"] = "center"
 
-title = html.H1(children="YACD - Yet Another COVID-19 dashboard", style=text_layout,)
-description = dcc.Markdown(
-    children="""
-        Yet another COVID-19 dashboard built by yet another data science enthusiast.  
-        I made this so I can look at the data the way I like.  
-        Please feel free to fork, modify and distribute.""",
-    style=layout_style,
-)
 
 header = dbc.Row(
-    [
-        dbc.Col(title, width={"size": 5}),
-        # dbc.Col(description, width={"size": "auto"}),
-    ],
-    align="center",
+    html.H1(children="YACD - Yet Another COVID-19 dashboard", style=text_layout,),
     justify="center",
-    style={"height": "10vh"},
+    style={"height": "8vh"},
 )
+footer = dbc.Row(dcc.Markdown(f"CSV last updated: {df.date.max().date()}"), justify="end")
 
 all_columns = df.columns.to_list()
 num_columns = df.select_dtypes([np.int64, np.float64, np.datetime64]).columns.to_list()
@@ -326,7 +208,7 @@ def unix_time_days(dt):
 
 body = dbc.Row(
     [
-        dbc.Col(side_bar, style={"width": "22vw"}),
+        dbc.Col([dbc.Row(side_bar),]),
         dbc.Col(
             [
                 dbc.Row(
@@ -350,7 +232,9 @@ body = dbc.Row(
     ],
 )
 
-app.layout = html.Div(children=[header, body], style={"background-color": "#F9F9F9"},)
+app.layout = html.Div(
+    children=dbc.Col([header, body, footer], width=12), style={"background-color": "#F9F9F9"},
+)
 server = app.server
 
 
